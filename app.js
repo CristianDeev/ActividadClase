8a00cad5ca65c0aa9b298798bdb3fd3406fa9108
CristianDeev <arboledaadvinculacristiancamil@gmail.com>
2024-10-07 09:09:13 -0500

    First Activity in JavaScript

---
+++ app.js
@@ -0,0 +1,9 @@
+var nombre = prompt("Cual es tu nombre?");
+var edad = prompt("Cuantos años tienes?");
+var ciudad = "Cali";
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Ciudad:", ciudad);
+
+alert(`Hola ${nombre}, tienes ${edad} años y vives en ${ciudad}.`);
